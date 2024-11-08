326dfe0708651e097ffad42a7fdaf63135bd902b
ktprasanth007 <ktprasanth007@gmail.com>
2024-11-09 01:16:00 +0530

    main directory change

--- LLD/src/main/java/org/lld/Step_07_DecoratorPattern/BasicPizza.java
+++ LLD/src/main/java/org/lld/Step_07_DecoratorPattern/BasicPizza.java
@@ -1,6 +1,6 @@
 package main.java.org.lld.Step_07_DecoratorPattern;
 
-public class BasicPizza implements Pizza {
+public class BasicPizza implements main.java.org.lld.Step_07_DecoratorPattern.Pizza {
     @Override
     public String getDescription() {
         return "Basic Pizza";
